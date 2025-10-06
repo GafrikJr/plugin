7aa8e07a77010d9a8e83ab5fb1e3aaeee76efc29
GafrikJr <gafrikjr22848@gmail.com>
2025-10-06 20:26:24 +0300

    comm

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,6 @@ import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.eclipse.jgit.treewalk.TreeWalk;
 
 import java.io.File;
@@ -15,26 +14,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) throws GitAPIException, IOException {
-        Git git = Git.open(new File("C:\\Users\\timur\\IdeaProjects\\GitTest\\.git"));
-        List<String> files = showBranchDiff(git, "master");
+        Git git = Git.open(new File("C:\\Users\\timur\\IdeaProjects\\dependencies-counter-maven-plugin\\.git"));
+        List<String> files = getFilesContent(git, "src/main/java/org/example");
         files.stream().filter(f -> f.contains("//TODO")).forEach(System.out::println);
 
     }
 
-    public static List<String> showBranchDiff(Git git, String branch1) throws IOException {
+    public static List<String> getFilesContent(Git git, String dirPath) throws IOException {
         List<String> files = new ArrayList<>();
         Repository repository = git.getRepository();
 
         RevCommit commit = repository.parseCommit(repository.resolve("HEAD"));
         RevTree tree = commit.getTree();
 
-        try (TreeWalk treeWalk = TreeWalk.forPath(repository, "src/main/java/org/example", tree)) {
+        try (TreeWalk treeWalk = TreeWalk.forPath(repository, dirPath, tree)) {
             if (treeWalk != null && treeWalk.isSubtree()) {
                 treeWalk.enterSubtree();
                 while (treeWalk.next()) {
